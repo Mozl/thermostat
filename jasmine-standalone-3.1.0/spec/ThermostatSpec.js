1d98596383501d35715e032b87116033a04466d8
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-04-18 15:48:56 +0100

    added features and tests for powersave on and off

--- jasmine-standalone-3.1.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/ThermostatSpec.js
@@ -8,19 +8,41 @@ describe('Thermostat', function(){
         it ('constructs a Thermostat object with a default temp of 20', function() {
             expect (a_thermostat.temp).toEqual(20);
         });
+
+        it ('constructs a Thermostat object with a min temp of 10', function() {
+            expect (a_thermostat.minimum).toEqual(10);
+        });
     });
 
     describe ('Increase temp function', function() {
         it ('increases temperature by 1', function() {
-            a_thermostat.upTemp()
+            a_thermostat.upTemp();
             expect (a_thermostat.temp).toEqual(21);
         });
     });
 
     describe ('Decrease temp function', function() {
         it ('decreases temperature by 1', function() {
-            a_thermostat.downTemp()
+            a_thermostat.downTemp();
             expect (a_thermostat.temp).toEqual(19);
+        });
+
+        it ('cannot go lower than minimum', function(){
+            a_thermostat.temp = 10;
+            a_thermostat.downTemp();
+            expect (a_thermostat.temp).toEqual(10);
+        });
+    });
+
+    describe ('Powersave', function() {
+        it ('when powersave mode is on, max temp is 25', function() {
+            a_thermostat.powersaveOn();
+            expect (a_thermostat.max).toEqual(25);
+        });
+
+        it ('when powersave mode is off, max temp is 32', function() {
+            a_thermostat.powersaveOff();
+            expect (a_thermostat.max).toEqual(32);
         });
     });
 });
--- jasmine-standalone-3.1.0/src/Thermostat.js
+++ jasmine-standalone-3.1.0/src/Thermostat.js
@@ -1,11 +1,27 @@
 function Thermostat() {
     this.temp = 20;
+    this.max = 25
+    this.minimum = 10
+    this.powersave = true
 }
 
 Thermostat.prototype.upTemp = function() {
-   this.temp += 1
+    this.temp += 1;
 };
 
 Thermostat.prototype.downTemp = function() {
-    this.temp -= 1
+    if (this.temp === this.minimum)
+        this.temp = 10;
+    else
+        this.temp -= 1;
+};
+
+Thermostat.prototype.powersaveOn = function() {
+    this.max = 25;
+    this.powersave = true
+};
+
+Thermostat.prototype.powersaveOff = function() {
+    this.max = 32;
+    this.powersave = false;
 };
